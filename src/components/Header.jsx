a439cb5905781619001b24a4f35c39eac2dd705d
ar4ibais <ak.hutov@yandex.ru>
2023-12-22 12:33:02 +0300

    got data with details of selected country

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 
 import { Container } from './Container';
 import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -44,6 +45,8 @@ export const Header = () => {
     dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
   }
 
+  const cleanUp = () => dispatch(clearControls())
+
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
   }, [theme]);
@@ -52,7 +55,7 @@ export const Header = () => {
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
+          <Title onClick={cleanUp}>Where is the world?</Title>
           <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size="14px" />
